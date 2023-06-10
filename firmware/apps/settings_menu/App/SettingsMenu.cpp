627d4920c78940d7d7499822d1e2416d31eecf96
Peter <rankstar59@gmail.com>
2023-06-10 22:28:16 +1000

    add: update link on bPod

--- firmware/apps/settings_menu/App/SettingsMenu.cpp
+++ firmware/apps/settings_menu/App/SettingsMenu.cpp
@@ -2,14 +2,21 @@
 
 #include <App/Brightness.hpp>
 #include <App/About.hpp>
+#include <App/QrCodeView.hpp>
 #include <stringdb.h>
 
 Brightness brightness;
 About about;
+QrCodeView update;
 
 void SettingsMenu::begin(BpodMenu &menu) {
     menu.set_title(STRING(STRING_SETTINGS));
     menu.add(STRING(STRING_BRIGHTNESS), [](){ App::manager_begin(brightness); });
     menu.add(STRING(STRING_REBOOT), [](){ esp_restart(); });
+    menu.add(STRING(STRING_UPDATE), [](){
+        update.set_title(STRING(STRING_UPDATE));
+        update.set_text(STRING(UPDATE_URL));
+        App::manager_begin(update);
+    });
     menu.add(STRING(STRING_ABOUT), [](){ App::manager_begin(about); });
 }
